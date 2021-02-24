edc15738269023fc44125152c3c3e9f0558e9c32
RupertXXX <70776356+RupertXXX@users.noreply.github.com>
2021-02-24 15:13:55 +0300

    Доработал пагинацию на странице friends и закоментил код с азделением users и friends

--- src/Components/Content/friends/friendsFunc.jsx
+++ src/Components/Content/friends/friendsFunc.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Counter from './counter/counter';
 import Onefriend from './onefriend/onefriend';
 import c from './friends.module.css';
 
 const FriendsFunc = (props) => {
-    let [friedsUsersToggle, setFriedsUsersToggle] = useState(true);
+    // let [friedsUsersToggle, setFriedsUsersToggle] = useState(true);
 
     let makeArrayAllUsers = () => {
         let friendsAllUsersData_is = props.friendsAllUsers.map (obj => 
@@ -19,57 +20,48 @@ const FriendsFunc = (props) => {
         return friendsAllUsersData_is;
     }
 
-    let makeArrayFriends = () => {
-        let friendsAllUsersData_is = props.friends.map (obj => 
-            <Onefriend name={obj.name} 
-                followed={obj.followed} 
-                photos={obj.photos} 
-                key={obj.id} 
-                id={obj.id} 
-                friendsFollowInProgress={props.friendsFollowInProgress} 
-                followFriend={props.followFriend} 
-                unfollowFriend={props.unfollowFriend} 
-            />);
-        return friendsAllUsersData_is;
-    }
-
-    let makeArrayCounter = () => {
-        let pageCount = Math.ceil(props.totalFriendsCount / props.pageSize);
-        let counter = [];
-        for(let i = 0; i < pageCount; i++)
-        {
-            if(i > 19) break;
-            counter[i] = i + 1;
-        }
-        let counter_is = counter.map(obj => 
-        <div key={obj} className={props.currentPage === obj ? c.selected : undefined} onClick={() => {props.newRequestOnClick(obj)} } >{obj}</div>);
-        return counter_is;
-    }
+    // let makeArrayFriends = () => {
+    //     let friendsAllUsersData_is = props.friends.map (obj => 
+    //         <Onefriend name={obj.name} 
+    //             followed={obj.followed} 
+    //             photos={obj.photos} 
+    //             key={obj.id} 
+    //             id={obj.id} 
+    //             friendsFollowInProgress={props.friendsFollowInProgress} 
+    //             followFriend={props.followFriend} 
+    //             unfollowFriend={props.unfollowFriend} 
+    //         />);
+    //     return friendsAllUsersData_is;
+    // }
 
     return(
         <div className={c.friends_main}>
             <div className={c.counter_btn}>
                 <div className={c.counter}>
-                    { makeArrayCounter() }
+                    <Counter currentPage={props.currentPage} 
+                             totalFriendsCount={props.totalFriendsCount}
+                             pageSize={props.pageSize}
+                             newRequestOnClick={props.newRequestOnClick} 
+                             portionSize={10} />
                 </div>
                 {
-                    friedsUsersToggle
-                    ?
-                        <button className={c.btn} onClick={() => setFriedsUsersToggle(prev => !prev)}>
-                            All users 
-                        </button>
-                    :
-                        <button className={c.btn} onClick={() => setFriedsUsersToggle(prev => !prev)}>
-                            Only friends
-                        </button>
+                    // friedsUsersToggle
+                    // ?
+                    //     <button className={c.btn} onClick={() => setFriedsUsersToggle(prev => !prev)}>
+                    //         All users 
+                    //     </button>
+                    // :
+                    //     <button className={c.btn} onClick={() => setFriedsUsersToggle(prev => !prev)}>
+                    //         Only friends
+                    //     </button>
                 }
             </div>
             {
-                friedsUsersToggle
-                ?
-                    makeArrayFriends()
-                :
-                    makeArrayAllUsers() /* <Onefriend /> */
+                // friedsUsersToggle
+                // ?
+                //     makeArrayFriends()
+                // :
+                makeArrayAllUsers() /* <Onefriend /> */
             }
         </div>
     );
